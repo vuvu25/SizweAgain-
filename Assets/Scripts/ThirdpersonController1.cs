01a2cb67e4b137361840a24f9651caee69b78066
VeetheDev <53603573+vuvu25@users.noreply.github.com>
2022-08-25 20:58:15 +0200

    Thirdpersoncontroller

---
+++ Assets/Scripts/ThirdpersonController1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdpersonController1 : MonoBehaviour
+{
+
+    public Camera MyCamera;
+    public float Speed = 5f;
+    public float RotationSpeed = 15f;
+    CharacterController MyController;
+
+    float mDesiredRotation = 0f;
+    void Start()
+    {
+        MyController = GetComponent<CharacterController>();
+    }
+
+
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(x, 0, z);
+
+        Vector3 rotatedMovement = Quaternion.Euler(0, MyCamera.transform.rotation.eulerAngles.y, 0) * movement;
+
+        MyController.Move(rotatedMovement * Speed * Time.deltaTime);
+
+        if (rotatedMovement.magnitude > 0)
+        {
+            mDesiredRotation = Mathf.Atan2(rotatedMovement.x, rotatedMovement.z) * Mathf.Rad2Deg;
+        }
+        Quaternion currentRotation = transform.rotation;
+        Quaternion targetRotation = Quaternion.Euler(0, mDesiredRotation, 0);
+        transform.rotation = Quaternion.Lerp(currentRotation, targetRotation, RotationSpeed * Time.deltaTime);
+
+    }
+}
+
