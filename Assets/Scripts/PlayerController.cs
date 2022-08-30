a28953dbeb377670634c357a69c9b7f1520e6a9b
VeetheDev <53603573+vuvu25@users.noreply.github.com>
2022-08-30 22:45:39 +0200

    update script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,11 @@ public class PlayerController : MonoBehaviour
 {
 
     public float moveSpeed;
+    public CharacterController charCon;
+
+    private Vector3 moveInput;
+
+    public Transform camTrans;
     
     void Start()
     {
@@ -15,6 +20,18 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
-        
+        moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        charCon.Move(moveInput);
+
+        charCon.Move(moveInput);
+
+        //control camera rotation
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+
+        camTrans.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
     }
 }
