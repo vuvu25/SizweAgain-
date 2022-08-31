03c98915e6abc2493cb89d08a2f7233dee28adee
VeetheDev <53603573+vuvu25@users.noreply.github.com>
2022-08-31 22:23:51 +0200

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,7 +11,9 @@ public class PlayerController : MonoBehaviour
     private Vector3 moveInput;
 
     public Transform camTrans;
-    
+    public float mouseSensitivity;
+    public bool invertX;
+    public bool invertY;
     void Start()
     {
         
@@ -20,18 +22,34 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
-        moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        //moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        //moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        Vector3 vertMove = transform.forward * Input.GetAxis("Vertical");
+
+        moveInput = vertMove * Time.deltaTime;
+
+
 
         charCon.Move(moveInput);
 
         charCon.Move(moveInput);
 
         //control camera rotation
-        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+        
+        if(invertX)
+        {
+            mouseInput.x = mouseInput.x;
+        }
+        if (invertY) 
+        {
+            mouseInput.y = -mouseInput.y;
+        }
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
 
         camTrans.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
+
+
     }
 }
