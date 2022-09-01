cbe2370bc776f8f9d110e3f318de1a5a8a32fa85
VeetheDev <53603573+vuvu25@users.noreply.github.com>
2022-09-01 11:58:08 +0200

    .

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //Making Camera Follow
+    
+    public Transform target;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void LateUpdate()
+    {
+        transform.position = target.position; 
+    }
+}
